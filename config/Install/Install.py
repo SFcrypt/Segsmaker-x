1a887d090df643540e48b55bdc115303ddc01fb8
Santiago <136390230+SFcrypt@users.noreply.github.com>
2026-06-12 02:40:02 -0600

    Update SM_Script URLs for venv.py

--- config/Install/Install.py
+++ config/Install/Install.py
@@ -39,7 +39,7 @@ iRON = os.environ
 
 def SM_Script(WEBUI):
     return [
-        f'https://github.com/SFcrypt/Segsmaker/raw/main/config/venv.py {WEBUI}',
+        f'https://github.com/SFcrypt/segsmaker-x/raw/main/script/SM/venv.py {WEBUI}',
         f'https://github.com/gutris1/segsmaker/raw/main/script/SM/Launcher.py {WEBUI}',
         f'https://github.com/gutris1/segsmaker/raw/main/script/SM/segsmaker.py {WEBUI}'
            ]
